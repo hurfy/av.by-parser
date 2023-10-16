68ccd39ff2ff42e2d8815d1ecac139fcdd755a48
Ilya Grobovskikh <hurfydev@gmail.com>
2023-10-17 00:02:32 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# AV.BY ASYNC PARSER
+This is an async parser that searches for auto information on a website [av.by](https://cars.av.by).
+
+*The [API](https://api.av.by/_doc) was used to compile the list of cars.
+
+## Feature:
+- Choosing a car brand
+- Selecting a car model
+- Displaying search results
+- Saving information to .json and .csv file
+
+## Quickstart:
+Clone the repository:
+```cmd
+  git clone git@github.com:CITGuru/PyInquirer.git
+```
+Install requirements:
+```cmd
+  pip install -r requirements.txt
+```
+Done, now just run the main.py file.
+
+## Libraries:
+- [PyInquirer](https://github.com/CITGuru/PyInquirer/) (Menu)
+- [Bs4](https://www.crummy.com/software/BeautifulSoup/) (Parsing)
+- [aiohttp](https://docs.aiohttp.org/en/stable/) (Async requests)
+- [requests](https://requests.readthedocs.io/en/latest/) (Requests)
+
+## Authors:
+- [@hurfy](https://github.com/hurfy)
---
+++ main.py
@@ -0,0 +1,151 @@
+from parser     import gather_cars_data, cars_count
+from PyInquirer import prompt
+from json       import load
+from examples   import custom_style_2
+from asyncio    import run
+from os.path    import exists
+from os         import makedirs
+from json       import dump
+from csv        import writer
+
+
+def load_cars_list() -> dict:
+    """
+    The function returns the dictionary of cars from cars.json
+    :return: dict: Auto dict
+    """
+    with open('cars.json', 'r', encoding='UTF-8') as file:
+        return load(file)
+
+
+def write_to_json(name: str, path: str, text: list) -> None:
+    """
+    The function writes the results to a .json file
+    :param name:  str: File name
+    :param path:  str: File path
+    :param text: list: Text to write
+    :return:           None
+    """
+    with open(f'{path}/{name}.json', 'w', encoding='UTF-8', newline='') as file:
+        dump([car.get_info() for car in text], file, ensure_ascii=False, indent=4)
+
+
+def write_to_csv(name: str, path: str, text: list) -> None:
+    """
+    The function writes the results to a .csv file
+    :param name:  str: File name
+    :param path:  str: File path
+    :param text: list: Text to write
+    :return:           None
+    """
+    with open(f'{path}/{name}.csv', 'w', encoding='UTF-8', newline='') as file:
+        csv_writer = writer(file, delimiter=';')
+
+        for car in text:
+            car = car.get_info()
+            csv_writer.writerow([car['model'], car['price'], car['params'], car['url']])
+
+
+def select_car() -> tuple:
+    """
+    The function creates a menu from a list of cars and their models,
+    allowing the user to conveniently select makes and models of cars
+    :return: tuple: (Brand, Brand ID, Model, Model ID)
+    """
+    cars_list = load_cars_list()
+
+    select_brand = [
+        {
+            'type': 'list',
+            'name': 'brand',
+            'message': 'Choose a car brand:',
+            'choices': [i for i in cars_list.keys()]
+        }
+    ]
+    selected_brand = prompt(select_brand, style=custom_style_2)['brand']
+
+    select_model = [
+        {
+            'type': 'list',
+            'name': 'model',
+            'message': 'Choose a model:',
+            'choices': [i for i in cars_list[selected_brand]['models']]
+        }
+    ]
+    selected_model = prompt(select_model, style=custom_style_2)['model']
+
+    return selected_brand, cars_list[selected_brand]['id'],\
+        selected_model, cars_list[selected_brand]['models'][selected_model]
+
+
+async def main() -> None:
+    """
+    The function starts an infinite loop, which can be terminated only by user's decision.
+    Creates a menu with auto and model selection, processes other inputs. Writes the results to files.
+    :return: None
+    """
+    while True:
+        brand, brand_id, model, model_id = select_car()
+        count = cars_count(brand_id, model_id)
+
+        # If the number of auto is 0, the search will not be performed
+        if count == 0:
+            continue_select = [
+                {
+                    'type': 'list',
+                    'name': 'bool',
+                    'message': f'0 cars found, want to continue?',
+                    'choices': ['Yes', 'No']
+                }
+            ]
+            would_continue = prompt(continue_select, style=custom_style_2)['bool']
+
+            if would_continue == 'Yes':
+                continue
+            else:
+                break
+
+        cars = await gather_cars_data(brand_id, model_id, count)
+        name = f'{brand.lower().replace(" ", "_")}_{model.lower().replace(" ", "_").replace("-", "_")}'
+        path = fr'output/{name}'
+
+        # Print the list of found cars
+        print_select = [
+            {
+                'type': 'list',
+                'name': 'bool',
+                'message': f'{len(cars)} cars found, show list?',
+                'choices': ['Yes', 'No']
+            }
+        ]
+        would_print = prompt(print_select, style=custom_style_2)['bool']
+
+        if would_print == 'Yes':
+            for car in cars:
+                print(f'\n{car}')
+
+        # Checking the folder
+        if not exists(path):
+            makedirs(path, exist_ok=True)
+
+        # Writing to files
+        write_to_json(name, path, cars)
+        write_to_csv(name, path, cars)
+
+        # Continue searching
+        continue_select = [
+            {
+                'type': 'list',
+                'name': 'bool',
+                'message': f'Want to continue?',
+                'choices': ['Yes', 'No']
+            }
+        ]
+        would_continue = prompt(continue_select, style=custom_style_2)['bool']
+
+        if would_continue == 'No':
+            break
+
+
+if __name__ == '__main__':
+    run(main())
